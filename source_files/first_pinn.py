2561dcdc4645e53b45770ad4d2ba684320cd6eff
Hamza B <66087630+Hamza-B1@users.noreply.github.com>
2023-11-23 21:00:23 +0000

    first commit

--- source_files/first_pinn.py
+++ source_files/first_pinn.py
@@ -1,3 +1,10 @@
 import numpy as np
 import torch
+import torch.nn as nn
+
+if torch.cuda.is_available():
+    print("yes")
+
+print(torch.__version__)
+
 
--- source_files/generate_synthetic_mri_data.py
+++ source_files/generate_synthetic_mri_data.py
@@ -1,12 +1,8 @@
 import numpy as np
 
-
-# imported CSV Format
-# Point ID | p | x | y | z | p magnitude | ux | uy | uz | u_magnitude
-
-def generate_synthetic_mri_data(path_to_cfd_csv_data: str,
-                                n: int, mean: float, std: float,
-                                output_path: str):
+def deresolution_and_add_noise(path_to_cfd_csv_data: str,
+                               n: int, mean: float, std: float,
+                               output_path: str):
     data = np.genfromtxt(path_to_cfd_csv_data, dtype=float, delimiter=',', names=True)
 
     # downsample by removing every nth element
@@ -15,16 +11,15 @@ def generate_synthetic_mri_data(path_to_cfd_csv_data: str,
     reduced_res = data[mask]
 
     # add Gaussian noise
-    for i in reduced_res:
-        noise = np.zeros(i.size)
-        i[5:8] += [np.random.normal(i[5], std),
-                   np.random.normal(i[6], std),
-                   np.random.normal(i[7], std)
-                   ]
+    for i in range(reduced_res.size):
+        data = reduced_res[i]
+        reduced_res[i][6] += np.random.normal(reduced_res[i][6], std)
+        reduced_res[i][7] += np.random.normal(reduced_res[i][7], std)
+        reduced_res[i][8] += np.random.normal(reduced_res[i][8], std)
 
-    np.savetxt(output_path, reduced_res, delimiter=',')
+    np.savetxt("../data/fdanozzle/nozzlecfd_with_noise.csv", reduced_res, delimiter=',')
 
 
-generate_synthetic_mri_data("../data/fdanozzle/nozzlecfd.csv",
-                            4, 0, 0.1,
-                            "..data/fdanozzle/nozzlecfd_with_noise.csv")
+deresolution_and_add_noise("../data/fdanozzle/nozzlecfd.csv",
+                           4, 0, 0.005,
+                           "..data/fdanozzle/nozzlecfd_with_noise.csv")
