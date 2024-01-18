81d8b8723a7adbd86e738e8c8ba0f91b75873cf9
Hamza B <hbacc2nd@gmail.com>
2024-01-18 20:57:27 +0000

    Revert "attempted different neural network code structure from tutorial"
    This reverts commit 44d813bc0810fa14a51d0e3f77e7225126a6507e.

--- source_files/import_data.py
+++ source_files/import_data.py
@@ -1,15 +1,24 @@
+import torch
 import pandas as pd
 
 
 def create_data_tensors_from_csv(path):
     data = pd.read_csv(path)
-    x = data["Points_0"].values
-    y = data["Points_1"].values
-    z = data["Points_2"].values
+    x = torch.tensor(data["Points_0"].values, requires_grad=True)
+    y = torch.tensor(data["Points_1"].values, requires_grad=True)
+    z = torch.tensor(data["Points_2"].values, requires_grad=True)
 
-    u = data["U_0"].values
-    v = data["U_1"].values
-    w = data["U_2"].values
-    p = data["p"].values
+    u = torch.tensor(data["U_0"].values, requires_grad=True)
+    v = torch.tensor(data["U_1"].values, requires_grad=True)
+    w = torch.tensor(data["U_2"].values, requires_grad=True)
+    p = torch.tensor(data["p"].values, requires_grad=True)
 
-    return x, y, z, u, v, w, p
+    inputs = torch.stack((x, y, z), dim=1)
+    labels = torch.stack((u, v, w, p), dim=1)
+
+    inputs.requires_grad_(True)
+    labels.requires_grad_(True)
+    return inputs, labels
+
+
+create_data_tensors_from_csv(f"../data/CFD_vtm/csv_data/r001.csv")
